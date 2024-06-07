c058121474397ed63905979cc972b4c2582acaf4
Patrick Drechsler <patrick@pdrechsler.de>
2024-06-07 23:41:17 +0200

    🔧 slides: update qr code script

--- presentation/scripts/create-qr-code-for-website.sh
+++ presentation/scripts/create-qr-code-for-website.sh
@@ -3,6 +3,4 @@
 # Invoke from this folder, otherwise change target location (`-o "..."`)
 # Also adopt URL (last parameter)...
 
-qrencode -s 6 -l H -o "../public/images/slides.png" "https://draptik.github.io/2024-05-adc-approval-testing/"
-
-qrencode -s 6 -l H -o "../public/images/sample-code.png" "https://github.com/draptik/2024-05-adc-approval-testing"
+qrencode -s 6 -l H -o "../public/images/slides.png" "https://draptik.github.io/2024-07-dwx24-mutation-testing/"
