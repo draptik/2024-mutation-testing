5def887895cfc8663de8426cb4e116e827aa6ae7
Patrick Drechsler <patrick@pdrechsler.de>
2024-06-11 00:37:30 +0200

    ♻️ refactor tests

--- code/stryker-demos/tests/StrykerDemos.MyLib.Tests/02_OrderProcessing/OrderProcessorTests.cs
+++ code/stryker-demos/tests/StrykerDemos.MyLib.Tests/02_OrderProcessing/OrderProcessorTests.cs
@@ -4,27 +4,17 @@ namespace StrykerDemos.MyLib.Tests._02_OrderProcessing;
 
 public class OrderProcessorTests
 {
-    [Fact]
-    public void Given_order_amount_below_100_returns_no_discount_applied()
-    {
-        var sut = new OrderProcessor();
-        var result = sut.ApplyDiscount(99);
-        result.Should().Be(99);
-    }
+    private readonly OrderProcessor _sut = new();
 
     [Fact]
-    public void Given_order_amount_between_100_and_500_returns_5_percent_discount_applied()
-    {
-        var sut = new OrderProcessor();
-        var result = sut.ApplyDiscount(200);
-        result.Should().Be(190); // 200 * 0.95
-    }
+    public void Given_order_amount_below_100_returns_no_discount_applied() 
+        => _sut.ApplyDiscount(99).Should().Be(99);
 
     [Fact]
-    public void Given_order_amount_above_500_returns_10_percent_discount_applied()
-    {
-        var sut = new OrderProcessor();
-        var result = sut.ApplyDiscount(600);
-        result.Should().Be(540); // 600 * 0.90
-    }
+    public void Given_order_amount_between_100_and_500_returns_5_percent_discount_applied() 
+        => _sut.ApplyDiscount(200).Should().Be(190); // 200 * 0.95
+
+    [Fact]
+    public void Given_order_amount_above_500_returns_10_percent_discount_applied() 
+        => _sut.ApplyDiscount(600).Should().Be(540); // 600 * 0.90
 }
